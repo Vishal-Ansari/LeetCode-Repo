85d680bd224be81c27d450ae159d50cbf3bcba2d
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2022-10-24 20:17:23 +0530

    Update 1335 Minimum Difficulty of a Job Schedule Hard.cpp

--- 1335 Minimum Difficulty of a Job Schedule Hard.cpp
+++ 1335 Minimum Difficulty of a Job Schedule Hard.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int n , maxVal = 1e7;
+    
+    
+        int n , maxVal = 1e7;
     int solve(vector<int>& jobs, int d , int index ,  vector<vector<int>> &dp){
         if(n==index && d==0)
             return 0; 
@@ -21,6 +23,34 @@ public:
         if(n<d)
             return -1; 
         vector<vector<int>> dp(n,vector<int>(d+1,-1));
-        return solve(jobs , d , 0 , dp); 
+        return solve(jobs , d , 0 , dp);
+    
+    
+//     int dp[301][11];
+    
+//         int solve(vector<int>& jobd, int n, int i, int d){
+//             if(d==1)
+//                 return *max_element(begin(jobd)+i,end(jobd));
+            
+//             if(dp[i][d]!=-1)
+//                 return dp[i][d];
+            
+//             int result=INT_MAX;
+//             int currd=INT_MIN;
+            
+//             for(int j=i;j<=n-d;j++){
+//                 currd=max(currd,jobd[j]);
+//                 result=min(result,currd+solve(jobd,n,j+1,d-1));
+//             }
+//             dp[i][d]=result;
+//             return dp[i][d];
+//         } 
+    
+//     int minDifficulty(vector<int> jobDifficulty, int d){
+//         int n=jobDifficulty.size();
+//         memset(dp,-1,sizeof(dp));
+//         if(n<d)
+//             return -1;
+//         return solve(jobDifficulty,n,0,d);
     }
 };
