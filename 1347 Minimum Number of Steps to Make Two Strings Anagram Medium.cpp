7a7fb3296150fec9aeef29f2da4c80a28a990725
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2022-11-02 11:47:55 +0530

    Update 1347 Minimum Number of Steps to Make Two Strings Anagram Medium.cpp

--- 1347 Minimum Number of Steps to Make Two Strings Anagram Medium.cpp
+++ 1347 Minimum Number of Steps to Make Two Strings Anagram Medium.cpp
@@ -2,6 +2,22 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         unordered_map<char,int>mp;
+        for(auto c:s) mp[c]++;
+        for(auto c: t) mp[c]--;
+        int ans=0;
+        for(auto a:mp) ans+=abs(a.second);
+        return ans/2;
+    }
+};
+
+
+
+
+
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        unordered_map<char,int>mp;
         
         int count=0;
          
