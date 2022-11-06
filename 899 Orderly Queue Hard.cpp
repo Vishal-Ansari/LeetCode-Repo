0ae5f563a201d4facffeee35eac3d61ac0243c76
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2022-11-06 12:13:03 +0530

    Create 899 Orderly Queue Hard.cpp

---
+++ 899 Orderly Queue Hard.cpp
@@ -0,0 +1,12 @@
+class Solution 
+{
+public:
+    string orderlyQueue(string s, int k) 
+    {
+        if (k > 1) { sort(s.begin(), s.end()); return s; }
+        string ss = s;
+        for (int i = 1; i < s.length(); ++i)
+            ss = min(ss, s.substr(i) + s.substr(0,i));
+        return ss;
+    }
+};
