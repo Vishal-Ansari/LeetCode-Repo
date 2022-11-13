f64768eb795f957ca195c9b455de0737fe7c428c
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2022-11-13 11:24:31 +0530

    Create 1318 Minimum Flips to Make a OR b Equal to c Medium.cpp

---
+++ 1318 Minimum Flips to Make a OR b Equal to c Medium.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int ans=0;
+        for(int i=0;i<32;i++){
+            bool x=false , y=false, z= false;
+            if(a & (1<<i ))
+            x= true;
+            if(b & (1<<i ))
+            y= true;
+            if(c & (1<<i ))
+            z= true;
+
+            if(z==false){
+                if(x==true && y==true)
+                ans+=2;
+                else if(x==true || y==true)
+                ans++;
+            }
+            else{
+                 if(x==false && y==false)
+                ans++;
+            }
+
+        }
+        return ans;
+    }
+};
