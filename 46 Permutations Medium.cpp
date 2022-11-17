1a9d7fa720f77b5deade2bbb4220f71feeef885e
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2022-11-17 14:36:18 +0530

    Update 46 Permutations Medium.cpp

--- 46 Permutations Medium.cpp
+++ 46 Permutations Medium.cpp
@@ -1,5 +1,34 @@
 class Solution {
 public:
+    void help(vector<int>& nums, int freq[],vector<vector<int>>&ans, vector<int>&ds ){
+        if(ds.size()==nums.size()){
+            ans.push_back(ds);
+            return ;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(!freq[i]){
+                ds.push_back(nums[i]);
+                freq[i]=1;
+                help(nums,freq,ans,ds);
+                freq[i]=0;
+                ds.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        int freq[nums.size()];
+        for(int i=0;i<nums.size();i++) freq[i]=0;        
+        help(nums,freq,ans,ds);
+        return ans;
+    }
+};
+
+
+
+class Solution {
+public:
     
     void help(vector<int>& nums, int i,vector<vector<int>>&ans ){
         if(i==nums.size()){
