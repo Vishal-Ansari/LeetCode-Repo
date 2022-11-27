a11252614f3fea26a0ea1a265e766d64711a4103
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2022-11-27 12:01:55 +0530

    Create 1626 Best Team With No Conflicts Medium.cpp

---
+++ 1626 Best Team With No Conflicts Medium.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        vector<vector<int>>v;
+        int n=scores.size();
+        for(int i=0;i<n;i++)  v.push_back({ages[i],scores[i]});
+        sort(v.begin(),v.end());
+        reverse(v.begin(),v.end());// sorted in decreasing order
+        // 4 5 5 6
+        // 2 1 1 2
+        int dp[n];
+        memset(dp,0,sizeof(dp));
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j>=0;j--){
+                if(v[i][1]<=v[j][1]){
+                    dp[i]=max(dp[i],dp[j]+v[i][1]);
+                }
+            }
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
