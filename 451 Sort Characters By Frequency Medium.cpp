f1457be7cf3ef3dfc0d12f3498b2be505e1858b1
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2022-12-04 00:49:59 +0530

    Create 451 Sort Characters By Frequency Medium.cpp

---
+++ 451 Sort Characters By Frequency Medium.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> freq;
+        vector<string> bucket(s.size()+1, "");
+        string res;
+        
+        
+        for(char c:s) freq[c]++;
+        //put character into frequency bucket
+        for(auto& it:freq) {
+            int n = it.second;
+            char c = it.first;
+            bucket[n].append(n, c);
+        }
+        //form descending sorted string
+        for(int i=s.size(); i>0; i--) {
+            if(!bucket[i].empty())
+                res.append(bucket[i]);
+        }
+        return res;
+    }
+};
