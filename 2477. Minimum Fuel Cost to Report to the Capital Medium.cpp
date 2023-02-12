322a111f5a989a7afa575f467aef8029ba360e20
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2023-02-12 13:34:39 +0530

    Create 2477. Minimum Fuel Cost to Report to the Capital Medium.cpp

---
+++ 2477. Minimum Fuel Cost to Report to the Capital Medium.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> edges;
+    vector<bool> visit;
+    long long ret = 0;
+    int count(int cur, int k){
+        int cnt = 1;
+        visit[cur] = true;
+        for(auto i: edges[cur]){
+            if(visit[i] == false)
+                cnt += count(i, k);
+        }
+        if( cur != 0)
+            ret += ((cnt + k -1) / k);
+        return cnt;
+    }
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        int n = roads.size();
+        edges.resize(n + 1);
+        visit.resize(n + 1);
+        for(auto road: roads){
+            edges[road[0]].push_back(road[1]);
+            edges[road[1]].push_back(road[0]);
+        }
+        count(0, seats);
+        return ret;
+    }
+};
