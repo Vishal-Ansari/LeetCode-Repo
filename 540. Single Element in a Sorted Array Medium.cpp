54353a3fcb409b1449ad59457441bca0c5d80d04
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2023-02-21 06:23:07 +0530

    Update 540. Single Element in a Sorted Array Medium.cpp

--- 540. Single Element in a Sorted Array Medium.cpp
+++ 540. Single Element in a Sorted Array Medium.cpp
@@ -6,3 +6,22 @@ public:
         return x;
     }
 };
+
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size(),l=0,r=n-1,m;
+            while(l<r){
+            m=l+(r-l)/2;
+            if(m%2==0){
+                if(nums[m]==nums[m+1]) l=m+1;
+                else {r=m;} }
+            else{
+                if(nums[m]!=nums[m+1]) l=m+1;
+                else {r=m;}
+            }
+            }
+            return nums[l];
+    }
+};
