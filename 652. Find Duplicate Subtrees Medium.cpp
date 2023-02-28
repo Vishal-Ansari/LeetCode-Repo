6c49a41193422c46b2f58f8872ff41f6287cc128
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2023-02-28 06:33:34 +0530

    Create 652. Find Duplicate Subtrees Medium.cpp

---
+++ 652. Find Duplicate Subtrees Medium.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    // Serialize subtrees and check for duplicates using a post-order traversal
+    string serializeSubtrees(TreeNode* node, unordered_map<string, int>& subtrees, vector<TreeNode*>& duplicates) {
+        if (!node) return "#"; // Null nodes are represented by '#'
+        
+        string left = serializeSubtrees(node->left, subtrees, duplicates);
+        string right = serializeSubtrees(node->right, subtrees, duplicates);
+        
+        string s = left + "," + right + "," + to_string(node->val); // Serialize the current subtree
+        
+        if (subtrees[s] == 1) duplicates.push_back(node); // If a duplicate subtree is found, add to the vector
+        
+        subtrees[s]++;
+        return s;
+    }
+ vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        unordered_map<string, int> subtrees; // Store serialized subtree and its frequency
+        vector<TreeNode*> duplicates; // Store duplicate subtrees
+        
+        serializeSubtrees(root, subtrees, duplicates); // Traverse the tree and serialize subtrees
+        
+        return duplicates;
+    }
+    
+};
