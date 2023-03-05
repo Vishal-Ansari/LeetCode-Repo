76a1228201cf655535d9315375a803bbf77ac130
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2023-03-05 08:08:25 +0530

    Create 923. 3Sum With Multiplicity Medium.cpp

---
+++ 923. 3Sum With Multiplicity Medium.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int threeSumMulti(vector<int>& arr, int target) {
+        unordered_map<int, int> m;
+        int res = 0, mod = 1e9 + 7;
+        for (int i = 0; i < arr.size(); i++) {
+            res = (res + m[target - arr[i]]) % mod;
+            for (int j = 0; j < i; j++) {
+                m[arr[i] + arr[j]]++;
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int threeSumMulti(int[] arr, int target) {
+        long result = 0;
+        long count[] = new long[101];
+        long mod = 1000000007;
+		
+		//Couting the occurrence of each number
+        for(int i: arr) count[i]++;
+        
+        for(int i=0;i<101;i++){
+            for(int j=i;j<101;j++){
+                int k = target - i -j;
+                if(k<0 || k>100)
+                    continue;
+				// if all number are same than use nC3
+                if(i==j && j==k){
+                    result += (count[i]*(count[i]-1)*(count[i]-2))/6;
+                }
+				
+				//if first two are same than use nC2 * occurence of k
+                else if(i==j && j!=k){
+                    result += ((count[i]*(count[i]-1))/2)*count[k];
+                }
+				//Last condition if all number ar different
+				else if(i<j && j<k){
+                    result += (count[i]*count[j]*count[k]);
+                }
+                result %= mod;
+            }
+        }
+        return (int)result;
+    }
+}
