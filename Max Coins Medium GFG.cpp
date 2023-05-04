e8ab833d2c8e1c6499a7ae0916b7a667105d8651
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2023-05-04 13:53:35 +0530

    Create Max Coins Medium GFG.cpp

---
+++ Max Coins Medium GFG.cpp
@@ -0,0 +1,68 @@
+//without comparater function
+class Solution{
+public:
+    int maxCoins(int n,vector<vector<int>> &ranges){
+        sort(ranges.begin(),ranges.end());
+        vector<int> dp(n);
+        dp[n - 1] = ranges[n - 1][2];
+        for(int i = n - 2; i > -1; i--)
+            dp[i] = max(dp[i + 1], ranges[i][2]);
+        int answer = 0;
+        for(int i = 0; i < n; i++){
+            int low= i;
+            int high = n;
+           while(low < high - 1){
+              int mid = low + (high - low) / 2;
+                
+                if(ranges[mid][0] >= ranges[i][1])
+                  high = mid;
+                else
+                  low = mid;
+            }
+            int cur = 0;
+            if(high < n)
+              cur = dp[high];
+            answer = max(answer, ranges[i][2] + cur);
+        }
+        
+      return answer;
+    }
+};
+
+
+
+// with comparator function 
+
+class Solution{
+public:
+    int maxCoins(int n,vector<vector<int>> &ranges){
+        sort(ranges.begin(), ranges.end(), [&](vector<int> &a, vector<int> &b){
+            if(a[0] == b[0])
+                return a[1] < b[1];
+            return a[0] < b[0];
+        });
+        vector<int> dp(n);
+        dp[n - 1] = ranges[n - 1][2];
+        for(int i = n - 2; i > -1; i--)
+            dp[i] = max(dp[i + 1], ranges[i][2]);
+        int answer = 0;
+        for(int i = 0; i < n; i++){
+            int low= i;
+            int high = n;
+           while(low < high - 1){
+              int mid = low + (high - low) / 2;
+                
+                if(ranges[mid][0] >= ranges[i][1])
+                  high = mid;
+                else
+                  low = mid;
+            }
+            int cur = 0;
+            if(high < n)
+              cur = dp[high];
+            answer = max(answer, ranges[i][2] + cur);
+        }
+        
+      return answer;
+    }
+};
