4c00236b5bed6320d7e2ced84b0df7b06cb2a347
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2023-06-19 14:31:37 +0530

    Create Rearrange an array with O(1) extra space GFG.cpp

---
+++ Rearrange an array with O(1) extra space GFG.cpp
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+    void arrange(long long arr[], int n) {
+        
+        for(int i=0;i<n;i++){
+            arr[i]=arr[i]+(arr[arr[i]]%n)*n;
+        }
+        for(int i=0;i<n;i++){
+            arr[i]/=n;
+        }
+    }
+};
