271eeb3b544e7d20d8e5b631ff466240cd5282c7
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2023-07-06 12:46:08 +0530

    Create Quick Sort medium GFG.cpp

---
+++ Quick Sort medium GFG.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+    public:
+    void quickSort(int arr[], int low, int high)
+    {
+         if (low < high)
+    {
+        int pivot = partition(arr, low, high);
+        
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+    }
+    
+    public:
+    int partition (int arr[], int low, int high)
+    {
+        int pivot = arr[high];
+        int i = low - 1;
+    
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    
+    return i + 1;
+    }
+};
