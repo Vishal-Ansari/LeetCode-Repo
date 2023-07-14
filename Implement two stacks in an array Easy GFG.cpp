17d6fb67995ad920ee8e2103430b4970ab75d88b
Vishal ansari <91402669+Vishal-Ansari@users.noreply.github.com>
2023-07-14 10:50:34 +0530

    Create Implement two stacks in an array Easy GFG.cpp

---
+++ Implement two stacks in an array Easy GFG.cpp
@@ -0,0 +1,41 @@
+class twoStacks
+{
+    int *arr;
+    int size;
+    int top1, top2;
+    public:
+    
+    twoStacks(int n=100)
+    {
+        size = n; 
+        arr = new int[n]; 
+        top1 = -1; 
+        top2 = size;
+    }
+ 
+    //Function to push an integer into the stack1.
+    void push1(int x)
+    {
+        arr[++top1]=x;
+    }
+    
+    //Function to push an integer into the stack2.
+    void push2(int x)
+    {
+       arr[--top2]=x;
+    }
+    
+    //Function to remove an element from top of the stack1.
+    int pop1()
+    {
+        if(top1==-1) return -1;
+        else return arr[top1--];
+    }
+    
+    //Function to remove an element from top of the stack2.
+    int pop2()
+    {
+       if(top2==size) return -1;
+       else return arr[top2++];
+    }
+};
